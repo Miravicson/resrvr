dcf07058a396e30f6f73bcd1d084761e28a5fcd1
Victor Ughonu <victorughonu95@gmail.com>
2020-04-30 15:48:30 +0100

    modified the elixir_buidpack.config, modified the Rsvp.Endpoint websockets configuration

--- apps/rsvp_web/lib/rsvp_web/endpoint.ex
+++ apps/rsvp_web/lib/rsvp_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule RsvpWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :rsvp_web
 
   socket "/socket", RsvpWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
